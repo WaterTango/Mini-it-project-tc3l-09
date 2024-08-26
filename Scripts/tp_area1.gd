b542bc1f12cb7a15ee5180368a076a15cc302fab
daniel <danielikmal1404@gmail.com>
2024-08-26 18:52:26 +0800

    level 2 improvements
    added tress and decors

--- Scripts/tp_area1.gd
+++ Scripts/tp_area1.gd
@@ -1,5 +1,6 @@
 extends CollisionShape2D
 
+@onready var start_level = preload("res://Scenes/Game_scene/world_2.tscn")
 
-func _on_tp_area_body_entered(body: CharacterBody2D) -> void:
+func _on_tp_area_body_entered(body: CharacterBody2D):
 	get_tree().change_scene_to_file("res://Scenes/Game_scene/world_2.tscn")
