ca65201be1004b3ddbaeeb1a99e3aafcd90330a8
he7yz <shazarulhafiz7@gmail.com>
2024-09-03 19:05:33 +0800

    added DS4 support
    minor changes no biggie

--- Scenes/settingsmenu/fp_scounter.gd
+++ Scenes/settingsmenu/fp_scounter.gd
@@ -2,11 +2,11 @@ extends Control
 
 @onready var label: Label = $FPSlayer/Label as Label
 
-# Called when the node enters the scene tree for the first time.
+# called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# called every frame, [delta] is the elapsed time since the previous frame
 func _process(delta: float) -> void:
 	label.text = str(Engine.get_frames_per_second())
