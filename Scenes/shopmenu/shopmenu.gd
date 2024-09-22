6448b1f8c7eabecb3b1ea371b48d9b9a56a6dc42
shaz <shazarulhafiz7@gmail.com>
2024-09-22 21:29:51 +0800

    Major UI Fixes
    Minimap is now working well, only in world 2 - 1 and 2 - 2 as it is in a "surface area".
    
    ShopMenu:
    - Animation is now smoother and faster
    - Added Outro pullout anim.
    - Potions can be interacted
    - Added extra weapons just for the looks
    - SFX for putting potion in cart (PotionBubbleSFX)
    
    Remade Quest UI. Made it look prettier with some fancy boxes and animated sprites for the checkbox. No more Fragment text UI on the bottom left.
    Repositioned the UI so that it alligns with minimap on the topleft and interactive DS4 gui on bottomright.

--- Scenes/shopmenu/shopmenu.gd
+++ Scenes/shopmenu/shopmenu.gd
@@ -11,6 +11,7 @@ var shop
 @onready var wep_1_button: Button = $ContainerExt/WeaponsCategory/Label/WeaponScroll/Node2D/weapon1/wep1button
 @onready var wep_2_button: Button = $ContainerExt/WeaponsCategory/Label/WeaponScroll/Node2D/weapon2/wep2button
 @onready var wep_3_button: Button = $ContainerExt/WeaponsCategory/Label/WeaponScroll/Node2D/weapon3/wep3button
+@onready var w_1: Control = $ContainerInfo/w1 as Control
 
 #potions
 @onready var greenpotbutton: Button = $ContainerExt/PotionsCategory/Label/PotionScroll/Node2D/Potion1/greenpotbutton
@@ -40,6 +41,7 @@ func handle_connecting_shop_signals() -> void:
 	wep_3_button.button_down.connect(_on_wep3_pressed)
 	pass
 	
+#weponz
 func _on_wep1_pressed():
 	pass
 
@@ -48,3 +50,55 @@ func _on_wep2_pressed():
 
 func _on_wep3_pressed():
 	pass
+
+#potionz
+func _on_greenpotbutton_pressed() -> void:
+	w_1.visible = false
+	$ContainerInfo/p2.visible = false
+	$ContainerInfo/p3.visible = false
+	$ContainerInfo/p4.visible = false
+	$ContainerInfo/p5.visible = false
+	$ContainerInfo/p1.visible = true
+	$PotionBubbleSFX.play()
+	
+	
+
+func _on_bluepotbutton_pressed() -> void:
+	w_1.visible = false
+	$ContainerInfo/p1.visible = false
+	$ContainerInfo/p3.visible = false
+	$ContainerInfo/p4.visible = false
+	$ContainerInfo/p5.visible = false
+	$ContainerInfo/p2.visible = true
+	$PotionBubbleSFX.play()
+
+func _on_purplepotbutton_pressed() -> void:
+	w_1.visible = false
+	$ContainerInfo/p1.visible = false
+	$ContainerInfo/p2.visible = false
+	$ContainerInfo/p4.visible = false
+	$ContainerInfo/p5.visible = false
+	$ContainerInfo/p3.visible = true
+	$PotionBubbleSFX.play()
+	
+
+func _on_yellowpotbutton_pressed() -> void:
+	w_1.visible = false
+	$ContainerInfo/p1.visible = false
+	$ContainerInfo/p2.visible = false
+	$ContainerInfo/p3.visible = false
+	$ContainerInfo/p5.visible = false
+	$ContainerInfo/p4.visible = true
+	$PotionBubbleSFX.play()
+	
+
+
+func _on_redpotbutton_pressed() -> void:
+	w_1.visible = false
+	$ContainerInfo/p1.visible = false
+	$ContainerInfo/p2.visible = false
+	$ContainerInfo/p3.visible = false
+	$ContainerInfo/p4.visible = false
+	$ContainerInfo/p5.visible = true
+	$PotionBubbleSFX.play()
+	
