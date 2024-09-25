3854d19626aa02ad127b09dd3db622f6ead023ef
daniel <danielikmal1404@gmail.com>
2024-09-25 17:27:17 +0800

    outtro+fixes
    added final credit scene
    remove debug code from every levels
    fixed ordering issue for settings

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -8,14 +8,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	Zoom()
+	pass
 	
 	
 
 
 #this is for debug remember to remove later
-func Zoom():
-	if Input.is_action_just_pressed("zoom-in") and OS.is_debug_build():
-		zoom = zoom * 1.1
-	if Input.is_action_just_pressed("zoom-out") and OS.is_debug_build():
-		zoom = zoom * 0.9
